efad72bb14bf6675d028455ae40827c513ffad80
Yun Zhang <yzhang175@hawk.iit.edu>
2021-04-12 19:23:10 +0800

    Run some basic diagonstics on fs

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require ('fs');
+const cheerio = require('cheerio');
+
+var raw_html = fs.readFileSync('data/raw/itmd.html');
+var $ = cheerio.load(raw_html);
+
+console.log("There are", $('.coursetitle').length,"ITMD courses.");
+
+$('.coursetitle').each(function()) {
+  console.log($(this).text());
+});
+// $('.courseblockdesc').text().replace(/\n/gm, "");
